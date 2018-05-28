191bbe2de0d3d7d54c4cd86237260ca60f349eea
bohdanka194 <bohdanka194@gmail.com>
2018-05-28 12:28:42 +0300

    view some info in home/index

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -13,11 +13,9 @@ namespace BookStore.Controllers
 
         public ActionResult Index()
         {
-            // получаем из бд все объекты Book
             IEnumerable<Book> books = db.Books;
-            // передаем все объекты в динамическое свойство Books в ViewBag
             ViewBag.Books = books;
-            // возвращаем представление
+
             return View();
         }
     }
