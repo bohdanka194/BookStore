e35b9c6b84639d10398c7caa88fb4c2a21dc848b
bohdanka194 <bohdanka194@gmail.com>
2018-05-30 11:07:29 +0300

    discovering methods in controller

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -33,5 +33,18 @@ namespace BookStore.Controllers
             db.SaveChanges();
             return "Спасибо," + purchase.Person + ", за покупку!";
         }
+        public string Square(int a = 10, int h = 3)
+        {
+            double s = a * h / 2.0;
+            return "<h2>Площадь треугольника с основанием " + a +
+                    " и высотой " + h + " равна " + s + "</h2>";
+        }
+        //public string Square()
+        //{
+        //    int a = Int32.Parse(Request.Params["a"]);
+        //    int h = Int32.Parse(Request.Params["h"]);
+        //    double s = a * h / 2.0;
+        //    return "<h2>Площадь треугольника с основанием " + a + " и высотой " + h + " равна " + s + "</h2>";
+        //}
     }
 }
